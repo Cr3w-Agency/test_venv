7165419295cabb80abbeb83266128ec93b37a794
Dmitriy Holovnia <cr3w.agency@gmail.com>
2020-07-23 21:47:10 +0300

    Initial commit

---
+++ mysite/news/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import News
+
+def index(request):
+    news = News.objects.order_by('-created_at')
+    context = {
+        'news': news,
+        'title': 'News list:',
+    }
+    return render(request, 'news/index.html', context)
+
+def test(request):
+    return HttpResponse('<h1>Test REQUEST</1>')
---
+++ test1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/dmitrijgolovna/Desktop/Python/test_venv/test1/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
